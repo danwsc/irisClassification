6f04c85436564b6cd0ed8924ae674880f0b796fa
Daniel Wong <suchunw@hotmail.com>
2021-05-29 12:20:19 +0800

    Continued debugging BS4

--- config/Settings.py
+++ config/Settings.py
@@ -7,13 +7,13 @@ class Settings:
     modelFile = "data/model.pkl" # machine learning model
 
     #Staging on local machine
-    # host='localhost'
-    # database='irisdb'
-    # user='root'
-    # password=''
+    host='localhost'
+    database='irisdb'
+    user='root'
+    password=''
 
     #Staging on heroku
-    host=os.environ['HOST']
-    database=os.environ['DATABASE']
-    user=os.environ['USERNAME']
-    password=os.environ['PASSWORD']
+    # host=os.environ['HOST']
+    # database=os.environ['DATABASE']
+    # user=os.environ['USERNAME']
+    # password=os.environ['PASSWORD']
